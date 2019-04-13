01021610463b00ec43838363436bea2afcd29ffc
Yaroslav Kostenko <yaroslavkostenko.yk@gmail.com>
2019-04-14 01:51:25 +0300

    datepicker

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import $ from 'jquery';
 
 window.jQuery = $;
 window.$ = $;
-
-
+var login;
 
 function signin(){
     var login = $("#signin_login").val();
@@ -18,8 +17,12 @@ function signin(){
             password:pass
            },
         success: function(data){
-            alert(data[0].login);
-            //window.location.href="index.html";
+            //$('#block2').hide();
+            alert("Sign in with login: " + data[0].login);
+            login = data[0].login;
+            window.location.href="calendar.html";
+            console.log(document.getElementById("login").val());
+            window.getElementById("login").innerHTML = login;
         },
         error: function() {
             alert("Error");
@@ -49,13 +52,21 @@ function signup(){
     });
 }
 
+function signout(){
+    window.location.href="index.html"
+}
+
 $(document).ready(function() {
-	$('.btn').click(function(event) {
-		$('.block').removeClass('hidden')
-		var num = $(this).attr('data-num');
-		$('#block'+num).addClass('hidden')
-	});
+  $('.btn').click(function(event) {
+    $('.block').removeClass('hidden')
+    var num = $(this).attr('data-num');
+    $('#block'+num).addClass('hidden')
+  });
 });
 
 $(document).on('click', '#signin', signin);
 $(document).on('click', '#signup', signup);
+$(document).on('click', '#signout', signout);
+
+
+         
